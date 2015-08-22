1a0632503a1bea320c30855bdb21a7686673e929
plasmaCheeseJoj <partlight.johans@gmail.com>
2015-08-22 08:38:12 +0200

    Added integrated assets to SandCat

--- src/com/partlight/sandcat/graphics/LinearGradient.java
+++ src/com/partlight/sandcat/graphics/LinearGradient.java
@@ -1,14 +1,9 @@
 package com.partlight.sandcat.graphics;
 
-import static com.badlogic.gdx.Gdx.graphics;
-import static com.badlogic.gdx.Gdx.gl;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
@@ -17,8 +12,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
 import com.partlight.sandcat.FragmentShaders;
-import com.partlight.sandcat.SandCatGame;
 import com.partlight.sandcat.VertexShaders;
+import com.partlight.sandcat.assets.SandCatAssets;
 
 public class LinearGradient extends Image implements Disposable {
 
@@ -31,11 +26,20 @@ public class LinearGradient extends Image implements Disposable {
 		this.fbFrameBuffer = this.createFrameBuffer();
 		this.setColor(Color.WHITE);
 		this.frameBuffer();
-		this.setSize(graphics.getWidth(), graphics.getHeight());
+		this.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	}
+
+	protected FrameBuffer createFrameBuffer() {
+		return new FrameBuffer(Format.RGBA8888, 1, Gdx.graphics.getHeight(), false);
 	}
 
 	protected ShaderProgram createShader() {
 		return new ShaderProgram(VertexShaders.vertDefault, FragmentShaders.fragGradient);
+	}
+
+	@Override
+	public void dispose() {
+		this.fbFrameBuffer.dispose();
 	}
 
 	protected void frameBuffer() {
@@ -43,25 +47,16 @@ public class LinearGradient extends Image implements Disposable {
 		final SpriteBatch batch = new SpriteBatch();
 
 		this.fbFrameBuffer.bind();
-		gl.glClearColor(0, 0, 0, 0);
-		gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.setShader(this.spShader);
-		batch.draw(((SandCatGame) Gdx.app.getApplicationListener()).getAssetLibrary().trPixel, 0, 0, 1, this.fbFrameBuffer.getHeight());
+		batch.draw(SandCatAssets.tPixel, 0, 0, 1, this.fbFrameBuffer.getHeight());
 		batch.end();
 		FrameBuffer.unbind();
 
 		this.setDrawable(new TextureRegionDrawable(new TextureRegion(this.fbFrameBuffer.getColorBufferTexture())));
 
 		batch.dispose();
-	}
-
-	protected FrameBuffer createFrameBuffer() {
-		return new FrameBuffer(Format.RGBA8888, 1, graphics.getHeight(), false);
-	}
-
-	@Override
-	public void dispose() {
-		this.fbFrameBuffer.dispose();
 	}
 }
